1bb9bbf73708c020f43c3b6353a0b34766495bf9
Dsb <paraderdsb@gmail.com>
2025-01-26 20:12:35 +0530

    installed preitier and create ui for task add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import {useState} from "react";
 
+const App = () => {
 
-const App=()=> {
+   const [task,setTask] = useState()
+
+    const handleInput=(event)=>{
+     setTask(event.target.value)
+    }
 
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hnpm install @reduxjs/toolkit react-reduxello world</h1>
-    </>
-  )
-}
+      <>
+    <div>
+      <div>
+        <label htmlFor="small-input" className=" mb-2 text-sm font-medium ">
+          Task
+        </label>
+        <input
+          type="text"
+          id="small-input"
+          name="task"
+          onChange={handleInput}
+          className="w-1/4 m-4 p-1 text-gray-900 border border-gray-300 rounded-lg   focus:ring-blue-500 "
+        />
+        <button
+          type="button"
+          className="text-white bg-gray-800  focus:ring-4  rounded-full text-sm px-5 py-2.5 me-2 mb-2  "
+        >
+          Add New Task
+        </button>
+      </div>
 
-export default App
+    </div>
+
+      <div>
+          {task}
+      </div>
+      </>
+  );
+};
+
+export default App;
