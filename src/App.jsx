0b6f000a05befabb467f254a7abe35cadae5fa56
Dsb <paraderdsb@gmail.com>
2025-01-26 19:23:24 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App=()=> {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <h1 className="text-3xl font-bold underline">Hnpm install @reduxjs/toolkit react-reduxello world</h1>
     </>
   )
 }
